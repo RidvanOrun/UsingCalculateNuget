5d99ca5a93b8eb30d5c4b38d08bc74ea8651f080
Rıdvan Örün <ridvan.orun@outlook.com>
2023-10-03 12:43:27 +0300

    Add project files.

---
+++ UsingCalculateNuget/Controllers/CalculateController.cs
@@ -0,0 +1,18 @@
+﻿using CalculateNugetExample;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsingCalculateNuget.Controllers
+{
+    [ApiController]
+    public class CalculateController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/calculate")]
+        public ActionResult Calculate(int val1, int val2)
+        {
+            var calculate = CalculateMath.Addition(val1,val2);
+
+            return Ok(calculate);
+        }
+    }
+}
